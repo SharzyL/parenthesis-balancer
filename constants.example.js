b680e3a3d80965319ef0e7d3c5b3e48425e331c2
SharzyL <me@sharzy.in>
2025-07-17 21:11:26 +0800

    bump wrangler

--- constants.example.js
+++ constants.example.js
@@ -1,6 +1,3 @@
-module.exports = {
-    API_URL: 'https://api.telegram.org',
-    WEBHOOK_URL: 'https://parenthesis-balancer.foobar.workers.dev',
-
-    TOKEN: '12345678:AAsdfjisdafbdfasidfasdafsdff',
-}
+export const API_URL = 'https://api.telegram.org'
+export const WEBHOOK_URL = 'https://parenthesis-balancer.foobar.workers.dev'
+export const TOKEN = '12345678:AAsdfjisdafbdfasidfasdafsdff'
